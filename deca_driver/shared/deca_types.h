c28bec4bcde5dd48e7b391c952f967a76982fa91
hig4342 <hig4342@naver.com>
2024-03-23 15:20:50 +0900

    refactor(decadriver): :art: change deca device api
    change macro to enum & update comments

--- deca_driver/shared/deca_types.h
+++ deca_driver/shared/deca_types.h
@@ -17,56 +17,15 @@
 extern "C" {
 #endif
 
-#ifndef uint8
-#ifndef _DECA_UINT8_
-#define _DECA_UINT8_
-typedef unsigned char uint8;
-#endif
-#endif
+#include <stdlib.h>
 
-#ifndef uint16
-#ifndef _DECA_UINT16_
-#define _DECA_UINT16_
-typedef unsigned short uint16;
-#endif
-#endif
-
-#ifndef uint32
-#ifndef _DECA_UINT32_
-#define _DECA_UINT32_
-typedef unsigned long uint32;
-#endif
-#endif
-
-#ifndef int8
-#ifndef _DECA_INT8_
-#define _DECA_INT8_
-typedef signed char int8;
-#endif
-#endif
-
-#ifndef int16
-#ifndef _DECA_INT16_
-#define _DECA_INT16_
-typedef signed short int16;
-#endif
-#endif
-
-#ifndef int32
-#ifndef _DECA_INT32_
-#define _DECA_INT32_
-typedef signed long int32;
-#endif
-#endif
-
-#ifndef NULL
-#define NULL ((void *)0UL)
-#endif
+typedef enum {
+  DWT_SUCCESS = 0,
+  DWT_ERROR   = -1
+} dwt_error_e;
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* DECA_TYPES_H_ */
-
-
